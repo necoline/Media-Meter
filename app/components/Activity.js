5ba88b85614b205762ed23ead3648ad9557e0869
necoline <hnhubner@gmail.com>
2017-01-10 21:51:45 -0700

    git addons

--- app/components/Activity.js
+++ app/components/Activity.js
@@ -15,7 +15,7 @@ message() {
 }
 
 image() {
-  return (<img src = {this.props.activity_attachment} />)
+  return (<img id="image" src = { this.props.activity_attachment } />)
 }
 
 content() {
@@ -26,15 +26,17 @@ render() {
   return (
    <div className="row">
     <div className="col s12 m6">
-      <div className="card blue-grey darken-1">
+      <div className="card indigo lighten-2">
       <div className="card-content white-text">
         <span className="card-title">
-        <img src = {this.provider()}/> <img src = {this.props.actor_avator}/></span>
-        <div> {this.props.actor_username}: {this.content()} </div>
+          <img id="provider" src = {this.provider()}/>
+        </span>
+        <div> <img id="avator" src = {this.props.actor_avator}/> {this.props.actor_username}: </div>
+        <div id="content">{this.content()}</div>
         <p>{this.props.activity_date}</p>
       </div>
       <div className="card-action">
-        <btn className="material-icons medium btn">thumb_up</btn>
+        <btn className="material-icons medium btn green accent-1">thumb_up</btn>
         <btn className="material-icons medium btn">chat_bubble_outline</btn>
         <btn className="material-icons medium btn btn-small">call_made</btn>
       </div>
